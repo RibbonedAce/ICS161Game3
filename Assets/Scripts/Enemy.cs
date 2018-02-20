a2e38c625da9b3a8ea8dfceeca44328cfaddead4
Tyler Gemora <tgemora@uci.edu>
2018-02-20 09:33:57 -0800

    Added shooting mechanic

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public int maxHealth;   // The maximum health of the enemy
+    public int health;      // The health of the enemy
+
+    void Awake ()
+    {
+        health = maxHealth;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    // Changes health of the enemy
+    public void ChangeHealth (int change)
+    {
+        health = Mathf.Min(Mathf.Max(0, health + change), maxHealth);
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    // Called when the enemy dies
+    public void Die ()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(Input.GetAxisRaw("Horizontal"), 0, 0);
+		if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.forward, 10);
+            if (hit.collider != null && hit.collider.CompareTag("Enemy"))
+            {
+                hit.collider.GetComponent<Enemy>().ChangeHealth(-1);
+            }
+        }
+	}
+}
