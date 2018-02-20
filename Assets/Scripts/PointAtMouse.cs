42f3f3cfaae9443829f25c8fd219dfd414568cce
Tyler Gemora <tgemora@uci.edu>
2018-02-19 17:26:34 -0800

    Put in a lighting test.

---
+++ Assets/Scripts/PointAtMouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAtMouse : MonoBehaviour {
+    private static Camera cam;  // The camera to use for reference
+
+    private void Awake()
+    {
+        if (cam == null)
+        {
+            cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 relativeCam = new Vector3(cam.transform.position.x, cam.transform.position.y, transform.position.z);
+        Vector3 mouseAt = new Vector3(cam.ScreenToWorldPoint(Input.mousePosition).x, cam.ScreenToWorldPoint(Input.mousePosition).y, 0);
+        transform.rotation = Quaternion.LookRotation(mouseAt - relativeCam, Vector3.back);
+	}
+}
