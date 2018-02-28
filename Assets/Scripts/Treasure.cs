11d6c5ef2468b8e26c7fdddd9d939878b08552dd
Tyler Gemora <tgemora@uci.edu>
2018-02-28 15:16:22 -0800

    ...

--- Assets/Scripts/Treasure.cs
+++ Assets/Scripts/Treasure.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Treasure : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    void OnCollisionEnter2D (Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            GameController.won = true;
+            Destroy(gameObject);
+        }
+    }
 }
