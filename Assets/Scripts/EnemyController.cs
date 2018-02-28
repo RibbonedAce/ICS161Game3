47b2a36cbaadb69d387cc533a78222b525a851a7
Tyler Gemora <tgemora@uci.edu>
2018-02-27 17:11:17 -0800

    Enemies fixed.

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController {
+    private static List<Vector2Int> taken = new List<Vector2Int>();  // The already occupied spots
+
+    // Return a new random vector not taken given a range
+    public static Vector2Int GetRandomVector (int min, int max)
+    {
+        Vector2Int result = new Vector2Int(Random.Range(min, max), Random.Range(min, max));
+        for (; taken.Contains(result); result = new Vector2Int(Random.Range(min, max), Random.Range(min, max)));
+        taken.Add(result);
+        return result;
+    }
+}
--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -231,7 +231,7 @@ public class Maze {
         {
             if(nodes[start].adjacents[Direction.Up] != null)
             {
-                visited.Add
+                //visited.Add
             }
         }
     }
