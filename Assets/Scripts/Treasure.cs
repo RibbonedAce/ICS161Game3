bb56ee702a97cfc9deff06625c532881c4cbbc73
Tyler Gemora <tgemora@uci.edu>
2018-03-15 14:29:39 -0700

    Treasure color.

--- Assets/Scripts/Treasure.cs
+++ Assets/Scripts/Treasure.cs
@@ -9,12 +9,15 @@ public class Treasure : MonoBehaviour {
     public GameObject afterEffect;      // The after-effect to use
     private Rigidbody2D _rigidbody2D;   // The Rigidbody component attached
     public Vector2Int pos;
-    public SpriteRenderer _spriteRender;
+    public SpriteRenderer _spriteRenderer;
+    public float colorRate;             // The rate of color change
+    private bool changing = false;
 
     void Awake ()
     {
         instance = this;
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Use this for initialization
@@ -26,12 +29,26 @@ public class Treasure : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        
+        if (!changing)
+        {
+            ChangeColor();
+        }
 	}
-    IEnumerator changeColor()
+
+    private IEnumerator ChangeColor ()
     {
-        yield return new WaitForEndOfFrame();
+        changing = true;
+        Color oldColor = _spriteRenderer.color;
+        Color newColor = RandomBrightColor();
+        for (float t = 0; t < colorRate; t += Time.deltaTime)
+        {
+            _spriteRenderer.color = Color.Lerp(oldColor, newColor, t / colorRate);
+            yield return null;
+        }
+        _spriteRenderer.color = newColor;
+        changing = false;
     }
+
     void OnCollisionEnter2D (Collision2D collision)
     {
         if (collision.collider.CompareTag("Player"))
@@ -48,5 +65,18 @@ public class Treasure : MonoBehaviour {
             MenuController.instance.GameLost();
             Destroy(gameObject);
         }
+    }
+
+    // Returns a random bright color
+    private Color RandomBrightColor ()
+    {
+        float r = Random.Range(0f, 1f);
+        float g = Random.Range(0f, 1f);
+        float b = Random.Range(0f, 1f);
+        float max = Mathf.Max(r, g, b);
+        r = r / max;
+        g = g / max;
+        b = b / max;
+        return new Color(r, g, b);
     }
 }
