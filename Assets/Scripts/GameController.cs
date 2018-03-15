f58a838621dfc99dd9cef2f9c037ca93b3732bfc
Tyler Gemora <tgemora@uci.edu>
2018-03-15 16:00:37 -0700

    Menu issues fixed.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,7 +6,7 @@ public static class GameController {
     public static int height = 10;                          // The height of everything
     public static int width = 10;                           // The width of everything
     public static GameStatus status = GameStatus.Playing;   // Whether the player has won/lost
-    public static Difficulty difficulty = Difficulty.Hard;  // The difficulty of the game
+    public static Difficulty difficulty = Difficulty.Easy;  // The difficulty of the game
     public static float volume = 0.5f;
     public static List<int> KillCountEnemy = new List<int>(3) { 0, 0, 0 };
 
---
+++ Assets/Scripts/SetDifficulty.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetDifficulty : MonoBehaviour {
+
+    void Awake ()
+    {
+        GetComponent<Dropdown>().value = (int)GameController.difficulty;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
